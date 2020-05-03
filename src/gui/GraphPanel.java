50566ecf88bf6da56a14646b98ecddbe7e76e2e7
Arjun Khanduri <arjun99.khanduri@gmail.com>
2020-05-03 10:17:57 +0530

    Update GraphPanel.java

--- src/gui/GraphPanel.java
+++ src/gui/GraphPanel.java
@@ -15,78 +15,78 @@ import java.util.List;
 
 public class GraphPanel extends JPanel implements MouseListener, MouseMotionListener {
 
-    private DrawUtils drawUtils;
+	private DrawUtils drawUtils;
 
-    private Graph graph;
+	private Graph graph;
 
-    private Node selectedNode = null;
-    private Node hoveredNode = null;
-    private Edge hoveredEdge = null;
+	private Node selectedNode = null;
+	private Node hoveredNode = null;
+	private Edge hoveredEdge = null;
 
-    private java.util.List<Node> path = null;
+	private java.util.List<Node> path = null;
 
-    private Point cursor;
+	private Point cursor;
 
-    public GraphPanel(Graph graph){
-        this.graph = graph;
+	public GraphPanel(Graph graph){
+		this.graph = graph;
 
-        addMouseListener(this);
-        addMouseMotionListener(this);
-    }
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
 
-    public void setPath(List<Node> path) {
-        this.path = path;
-        hoveredEdge = null;
-        repaint();
-    }
+	public void setPath(List<Node> path) {
+		this.path = path;
+		hoveredEdge = null;
+		repaint();
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
-        Graphics2D graphics2d = (Graphics2D) g;
-        graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        graphics2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		Graphics2D graphics2d = (Graphics2D) g;
+		graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-        drawUtils = new DrawUtils(graphics2d);
+		drawUtils = new DrawUtils(graphics2d);
 
-        if(graph.isSolved()){
-            drawUtils.drawPath(path);
-        }
+		if(graph.isSolved()){
+			drawUtils.drawPath(path);
+		}
 
-        if(selectedNode != null && cursor != null){
-            Edge e = new Edge(selectedNode, new Node(cursor));
-            drawUtils.drawEdge(e);
-        }
+		if(selectedNode != null && cursor != null){
+			Edge e = new Edge(selectedNode, new Node(cursor));
+			drawUtils.drawEdge(e);
+		}
 
-        for(Edge edge : graph.getEdges()){
-            if(edge == hoveredEdge)
-                drawUtils.drawHoveredEdge(edge);
-            drawUtils.drawEdge(edge);
-        }
+		for(Edge edge : graph.getEdges()){
+			if(edge == hoveredEdge)
+				drawUtils.drawHoveredEdge(edge);
+			drawUtils.drawEdge(edge);
+		}
 
-        for(Node node : graph.getNodes()){
-            if(node == selectedNode || node == hoveredNode)
-                drawUtils.drawHalo(node);
-            if(graph.isSource(node))
-                drawUtils.drawSourceNode(node);
-            else if(graph.isDestination(node))
-                drawUtils.drawDestinationNode(node);
-            else if(graph.isRedZoneNode(node))
-            	drawUtils.drawRedZoneNode(node);
+		for(Node node : graph.getNodes()){
+			if(node == selectedNode || node == hoveredNode)
+				drawUtils.drawHalo(node);
+			if(graph.isSource(node))
+				drawUtils.drawSourceNode(node);
+			else if(graph.isDestination(node))
+				drawUtils.drawDestinationNode(node);
+			else if(graph.isRedZoneNode(node))
+				drawUtils.drawRedZoneNode(node);
 			else if(graph.isHospitalNode(node))
 				drawUtils.drawHospitalNode(node);
-            else
-                drawUtils.drawNode(node);
-        }
-    }
+			else
+				drawUtils.drawNode(node);
+		}
+	}
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    	Node selected = null;
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		MainWindow.setTopPanel();
+		Node selected = null;
 		for(Node node : graph.getNodes()) {
 			if(DrawUtils.isWithinBounds(e, node.getCoord())){
 				selected = node;
@@ -99,6 +99,7 @@ public class GraphPanel extends JPanel implements MouseListener, MouseMotionList
 				graph.deleteNode(selected);
 				graph.setSolved(false);
 				repaint();
+				MainWindow.setTopPanel();
 				return;
 			} else if(e.isControlDown() && graph.isSolved()){
 				path = selected.getPath();
@@ -114,6 +115,7 @@ public class GraphPanel extends JPanel implements MouseListener, MouseMotionList
 						else
 							JOptionPane.showMessageDialog(null, "Destination can't be set as the Source","Warning",JOptionPane.WARNING_MESSAGE);
 					}
+					MainWindow.setTopPanel();
 				} else if(SwingUtilities.isRightMouseButton(e)) {
 					if(graph.isRedZoneNode(selected))
 						JOptionPane.showMessageDialog(null,"A red zone area can't be marked as the destination","Warning",JOptionPane.WARNING_MESSAGE);
@@ -124,6 +126,7 @@ public class GraphPanel extends JPanel implements MouseListener, MouseMotionList
 							graph.setDestination(selected);
 						else
 							JOptionPane.showMessageDialog(null, "Source can't be set as Destination","Warning",JOptionPane.WARNING_MESSAGE);
+						MainWindow.setTopPanel();
 					}
 				} 				else
 					return;
@@ -140,6 +143,7 @@ public class GraphPanel extends JPanel implements MouseListener, MouseMotionList
 						graph.setHospitalNode(selected);
 					else
 						graph.removeHospitalNode(selected);
+					MainWindow.setTopPanel();
 				}
 				else if(SwingUtilities.isRightMouseButton(e)) {
 					if(!graph.isRedZoneNode(selected)) {
@@ -150,6 +154,7 @@ public class GraphPanel extends JPanel implements MouseListener, MouseMotionList
 					}
 					else
 						graph.removeRedZone(selected);
+					MainWindow.setTopPanel();
 				}
 
 				graph.setSolved(false);
@@ -158,124 +163,125 @@ public class GraphPanel extends JPanel implements MouseListener, MouseMotionList
 			}
 		}
 
-        if(hoveredEdge!=null){
-            if(e.isControlDown() && e.isShiftDown()){
-                graph.getEdges().remove(hoveredEdge);
-                hoveredEdge = null;
-                graph.setSolved(false);
-                repaint();
-                return;
-            }
+		if(hoveredEdge!=null){
+			if(e.isControlDown() && e.isShiftDown()){
+				graph.getEdges().remove(hoveredEdge);
+				hoveredEdge = null;
+				graph.setSolved(false);
+				repaint();
+				return;
+			}
 
-            String input = JOptionPane.showInputDialog("Enter weight for " + hoveredEdge.toString()
-                                                        + " : ");
-            try {
-                int weight = Integer.parseInt(input);
-                if (weight > 0) {
-                    hoveredEdge.setWeight(weight);
-                    graph.setSolved(false);
-                    repaint();
-                } else {
-                    JOptionPane.showMessageDialog(null, "Weight should be positive");
-                }
-            } catch (NumberFormatException nfe) {}
-            return;
-        }
+			String input = JOptionPane.showInputDialog("Enter weight for " + hoveredEdge.toString()
+			+ " : ");
+			try {
+				int weight = Integer.parseInt(input);
+				if (weight > 0) {
+					hoveredEdge.setWeight(weight);
+					graph.setSolved(false);
+					repaint();
+				} else {
+					JOptionPane.showMessageDialog(null, "Weight should be positive");
+				}
+			} catch (NumberFormatException nfe) {}
+			return;
+		}
 
-        for(Node node : graph.getNodes()) {
-            if(DrawUtils.isOverlapping(e, node.getCoord())){
-                JOptionPane.showMessageDialog(null, "Overlapping Node can't be created");
-                return;
-            }
-        }
+		for(Node node : graph.getNodes()) {
+			if(DrawUtils.isOverlapping(e, node.getCoord())){
+				JOptionPane.showMessageDialog(null, "Overlapping Node can't be created");
+				return;
+			}
+		}
 
-        graph.addNode(e.getPoint());
-        graph.setSolved(false);
-        repaint();
-    }
+		graph.addNode(e.getPoint());
+		graph.setSolved(false);
+		repaint();
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+	@Override
+	public void mousePressed(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        for (Node node : graph.getNodes()) {
-            if(selectedNode !=null && node!= selectedNode && DrawUtils.isWithinBounds(e, node.getCoord())){
-                Edge new_edge = new Edge(selectedNode, node);
-                graph.addEdge(new_edge);
-                graph.setSolved(false);
-            }
-        }
-        selectedNode = null;
-        hoveredNode = null;
-        repaint();
-    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		for (Node node : graph.getNodes()) {
+			if(selectedNode !=null && node!= selectedNode && DrawUtils.isWithinBounds(e, node.getCoord())){
+				Edge new_edge = new Edge(selectedNode, node);
+				graph.addEdge(new_edge);
+				graph.setSolved(false);
+			}
+		}
+		selectedNode = null;
+		hoveredNode = null;
+		repaint();
+	}
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+	@Override
+	public void mouseEntered(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+	@Override
+	public void mouseExited(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        hoveredNode = null;
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		hoveredNode = null;
 
-        for (Node node : graph.getNodes()) {
-            if(selectedNode ==null && DrawUtils.isWithinBounds(e, node.getCoord())){
-                selectedNode = node;
-            } else if(DrawUtils.isWithinBounds(e, node.getCoord())) {
-                hoveredNode = node;
-            }
-        }
+		for (Node node : graph.getNodes()) {
+			if(selectedNode ==null && DrawUtils.isWithinBounds(e, node.getCoord())){
+				selectedNode = node;
+			} else if(DrawUtils.isWithinBounds(e, node.getCoord())) {
+				hoveredNode = node;
+			}
+		}
 
-        if(selectedNode !=null){
-            if(e.isControlDown()){
-                selectedNode.setCoord(e.getX(), e.getY());
-                cursor = null;
-                repaint();
-                return;
-            }
+		if(selectedNode !=null){
+			if(e.isControlDown()){
+				selectedNode.setCoord(e.getX(), e.getY());
+				cursor = null;
+				repaint();
+				return;
+			}
 
-            cursor = new Point(e.getX(), e.getY());
-            repaint();
-        }
-    }
+			cursor = new Point(e.getX(), e.getY());
+			repaint();
+		}
+	}
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
+	@Override
+	public void mouseMoved(MouseEvent e) {
 
-        if(e.isControlDown()){
-            hoveredNode = null;
-            for (Node node : graph.getNodes()) {
-                if(DrawUtils.isWithinBounds(e, node.getCoord())) {
-                    hoveredNode = node;
-                }
-            }
-        }
+		if(e.isControlDown()){
+			hoveredNode = null;
+			for (Node node : graph.getNodes()) {
+				if(DrawUtils.isWithinBounds(e, node.getCoord())) {
+					hoveredNode = node;
+				}
+			}
+		}
 
-        hoveredEdge = null;
+		hoveredEdge = null;
 
-        for (Edge edge : graph.getEdges()) {
-            if(DrawUtils.isOnEdge(e, edge)) {
-                hoveredEdge = edge;
-            }
-        }
+		for (Edge edge : graph.getEdges()) {
+			if(DrawUtils.isOnEdge(e, edge)) {
+				hoveredEdge = edge;
+			}
+		}
 
-        repaint();
-    }
+		repaint();
+	}
 
-    public void reset(){
-        graph.clear();
-        selectedNode = null;
-        hoveredNode = null;
-        hoveredEdge = null;
-        repaint();
-    }
+	public void reset(){
+		graph.clear();
+		selectedNode = null;
+		hoveredNode = null;
+		hoveredEdge = null;                                                                                                                                       
+		MainWindow.setTopPanel();
+		repaint();
+	}
 }
